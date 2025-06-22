62915a52d69008b624e3efea8d625c4359418db7
mohammed noorulameen <noorulameen8606@gmail.com>
2025-06-22 23:58:45 +0530

    change netflix link

--- src/Componets/Highlights/Projects/Projects.tsx
+++ src/Componets/Highlights/Projects/Projects.tsx
@@ -38,7 +38,7 @@ const Projects: React.FC = () => {
       id: 4,
       title: "netflix clone",
       description: "React netflix clone  ",
-      liveLink: "https://netflix-clone-pcermdq85-mohammednoorulameens-projects.vercel.app/",
+      liveLink: "netflix-clone-git-main-mohammednoorulameens-projects.vercel.app",
       sourceCode: "https://github.com/mohammednoorulameen/netflix-clone",
       smallImage: `${netflix}`, // Add small image URL
     },
