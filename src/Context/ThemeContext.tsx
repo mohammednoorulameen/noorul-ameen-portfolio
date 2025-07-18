b1e9527fbc9d21039e033686f3ee015551cc5a1c
noorulameen8606 <noorulameen8606@gmail.com>
2025-07-18 18:20:57 +0530

    change resume

--- src/Context/ThemeContext.tsx
+++ src/Context/ThemeContext.tsx
@@ -48,36 +48,3 @@ export const useTheme = () => {
 
 
 
-
-// import {createContext, useState, useContext, ReactNode} from 'react'
-
-
-
-// interface ThemeContextType{
-//     isDarkMode:boolean;
-//     toggleTheme:()=>void
-// }
-
-// const ThemeContext = createContext<ThemeContextType|undefined>(undefined);
-
-// export const ThemeProvider = ({children}: {children:ReactNode}) => {
-//     const [isDarkMode,setIsDarkMode] = useState<boolean>(false);
-
-//     const toggleTheme=()=>{
-//         setIsDarkMode((prev)=> !prev)
-//     }
-//     return(
-//         <ThemeContext.Provider value={{isDarkMode,toggleTheme}}>
-//             {children}
-//         </ThemeContext.Provider>
-//     )
-// }
-
-
-// export const useTheme = () =>{
-//     const context = useContext(ThemeContext);
-//     if(!context){
-//         throw new Error("useTheme must be used within a ThemeProvider")
-//     }
-//     return context;
-// }
